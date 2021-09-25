28aa1e02a485cb83e5d100fbddb96b1a6c7a1f80
dipsthedestroya <dipupurecha01@gmail.com>
2021-09-25 15:51:00 +0400

    Added reset functionality

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
 function createGrid(noOfBoxesPerSide){
-    let sideOfCanvas=640;
-    let sideOfBox=sideOfCanvas/noOfBoxesPerSide;
     for(let i=0;i<Math.pow(noOfBoxesPerSide,2);i++){
         const div=document.createElement("div");
-        div.setAttribute("style",`height:${sideOfBox}px;width:${sideOfBox}px;`);
+        div.setAttribute("style",`height:${100/noOfBoxesPerSide}%;width:${100/noOfBoxesPerSide}%;`);
         content.appendChild(div);
     }
-    console.log(content);
-
 }
+
+function createGridListener(){
+    grid=content.querySelectorAll('div');
+    grid.forEach((box)=>{
+        box.addEventListener('mouseover',()=>{
+            box.classList.add('hover');
+        })
+    })
+}
+
+function createButtonListener(button){
+    button.addEventListener('click',()=>{
+        let size;
+        grid.forEach((box)=>{
+            box.classList.remove("hover");
+            content.removeChild(box);
+        })
+        do{
+            size=prompt("Select size");
+        }while(size>100)
+        createGrid(size);
+        createGridListener();
+    
+    })
+}
+
 const content=document.querySelector('.content');
 const button=document.querySelector('button');
-createGrid(9);
 let grid=content.querySelectorAll('div');
-button.addEventListener('click',()=>{
-    console.log(grid);
-    grid.forEach((box)=>{
-        box.classList.remove("hover");
-        content.removeChild(box);
-    })
-    let side=prompt("Select size");
-    createGrid(side);
-})
 
-grid.forEach((box)=>{
-    box.addEventListener('mouseover',()=>{
-        box.classList.add('hover');
-        console.log("hello");
-    })
-})
+createGrid(16);
+createButtonListener(button);
+createGridListener();
 
